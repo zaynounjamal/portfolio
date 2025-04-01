683cdeb3d44e988528acc9ee603964b94ff90701
zaynounjamal <zaynounjamal77@gmail.com>
2025-04-01 12:15:36 +0300

    Configured for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base:"/portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
